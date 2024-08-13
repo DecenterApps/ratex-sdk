76c10839138240e1e7145a44e1469ca6438fda57
Skocimis <aki.spremo@gmail.com>
2024-08-13 17:22:58 +0200

    buidl

--- index.ts
+++ index.ts
@@ -2,31 +2,37 @@ import Web3 from "web3"
 import { findRoute } from "./routing/main"
 import { fetchPoolsData } from "./swap/graph_communication"
 import { Pool, Quote } from "./types"
-import { RegisteredSubscription } from "web3/lib/commonjs/eth.exports"
 
+export enum Dexes {
+    UNISWAP_V2 = "UniswapV2",
+    UNISWAP_V3 = "UniswapV3",
+    SUSHISWAP_V2 = "Sushiswap",
+    BALANCER = "Balancer",
+    CAMELOT = "Camelot"
+}
 interface RateXConfig {
     rpcUrl: string,
     chainId: number,
-    dexes: Array<string>,
+    dexes?: Array<Dexes>,
     graphApiKey: string
 }
 
-class RateX {
-    rpcProvider: Web3<RegisteredSubscription>
+export class RateX {
+    rpcProvider: Web3
     chainId: number
     graphApiKey: string
-
+    dexes: Array<Dexes>
 
     constructor(config: RateXConfig) {
         this.rpcProvider = new Web3(new Web3.providers.HttpProvider(config.rpcUrl))
         this.chainId = config.chainId
         this.graphApiKey = config.graphApiKey
-
+        this.dexes = config.dexes || [Dexes.SUSHISWAP_V2, Dexes.UNISWAP_V2, Dexes.UNISWAP_V3, Dexes.BALANCER, Dexes.CAMELOT]
     };
 
     async getQuote(tokenIn: string, tokenOut: string, amountIn: bigint): Promise<Quote> {
         const preFetchPools = Date.now();
-        const pools: Pool[] = await fetchPoolsData(tokenIn, tokenOut, 5, 5, this.chainId, this.rpcProvider, this.graphApiKey)
+        const pools: Pool[] = await fetchPoolsData(tokenIn, tokenOut, 5, 5, this.chainId, this.rpcProvider, this.graphApiKey, this.dexes)
         const middle = Date.now();
         const route = await findRoute(tokenIn, tokenOut, amountIn, pools, this.chainId)
         const finished = Date.now();
@@ -34,5 +40,3 @@ class RateX {
         return route
     }
 }
-
-export default RateX
