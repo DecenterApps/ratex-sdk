97610e4684b7b3f1e3fb336cb535637d216a0b73
vaniiiii <ivandzambas@gmail.com>
2024-08-14 18:30:53 +0200

    Balancer fix

--- dexes/pools/Balancer/BalancerState.ts
+++ dexes/pools/Balancer/BalancerState.ts
@@ -18,7 +18,7 @@ export class BalancerState {
                     .getWeightedPoolInfo(pool.poolId)
                     .call();
                 
-                const [decimals, invariant, tokens, balances, weights, swapFeePercentage] = [res[0], res[1], res[2], res[3], res[4], res[5]];
+                const [decimals, tokens, balances, weights, swapFeePercentage] = [res[0], res[1], res[2], res[3], res[4]];
                 const weightedPool = new BalancerWeightedPool(pool.poolId, pool.dexId, pool.tokens, balances, weights, swapFeePercentage);
                 return weightedPool;
             } catch (err) {
